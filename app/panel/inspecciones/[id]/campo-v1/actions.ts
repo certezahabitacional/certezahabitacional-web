ce1b5289af12cc4993498df22f6965fe5a67b4f7
certezahabitacional <ing.celedoniogil@gmail.com>
2026-09-15 18:51:59 -0600

    Consolida acciones de campo para V1

---
+++ app/panel/inspecciones/[id]/campo-v1/actions.ts
@@ -0,0 +1,8 @@
+"use server";
+
+export {
+  inicializarPlanAreasV3 as inicializarPlanAreasV1,
+  marcarPuntoNoAplicaV3 as marcarPuntoNoAplicaV1,
+  agregarPuntoInspectorV3 as agregarPuntoInspectorV1,
+  cerrarAreaSinHallazgosV3 as cerrarAreaSinHallazgosV1,
+} from "../campo-v3/actions";
